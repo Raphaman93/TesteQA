16df52b1ce394d7fdb57f6e5f1eef7c8b2b71c50
Rapha <manfresp@hotmail.com>
2021-02-05 11:21:22 -0300

    Writing steps definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Dado('que o usuário está na home page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('insere os dados no campo de busca') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('clica no botão de busca') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
